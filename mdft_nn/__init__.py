f8db75395bbeab5971a481d54e87dbd193bdc79a
taher <taher.rahgooy@gmail.com>
2021-04-15 08:52:10 -0500

    some changes in packaging

---
+++ mdft_nn/__init__.py
@@ -0,0 +1,3 @@
+import mdft_nn.dft_net
+import mdft_nn.trainer
+import mdft_nn.trainer_helpers
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Rahgooy/MDFT",
     python_requires=">=3.7",
-    packages=find_packages(),
+    packages=['mdft', 'mdft_nn'],
     license="",
     install_requires=install_requires,
     extras_require={
