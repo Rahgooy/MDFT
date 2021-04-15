9a41da5f0ca205f69e84cdfe1cb60c0baf7ca4eb
taher <taher.rahgooy@gmail.com>
2021-04-15 08:56:06 -0500

    bug fixed

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Rahgooy/MDFT",
     python_requires=">=3.7",
-    packages=[find_packages()],
+    packages=find_packages(),
     license="",
     install_requires=install_requires,
     extras_require={
